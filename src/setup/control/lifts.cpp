81e7a5ac3c9b8b596bac2b47ae5d8a9aa6627d0e
narayan99883 <robogearheads@gmail.com>
2022-01-06 16:54:36 -0500

    Update lifts.cpp

--- src/setup/control/lifts.cpp
+++ src/setup/control/lifts.cpp
@@ -2,7 +2,7 @@
 #include "setup/util/misc.h"
 
 pros::Motor FrontLift(11, MOTOR_GEARSET_36, 1, MOTOR_ENCODER_DEGREES);
-pros::Motor BackLift(13, MOTOR_GEARSET_36, 0, MOTOR_ENCODER_DEGREES);
+pros::Motor BackLift(13, MOTOR_GEARSET_36, 0, MOTOR_ENCODER_DEGREES); //port 13
 
 pros::ADIDigitalOut Claw ('A');
 
